29517f4d5904f25554a8e0dec7b54a5172513beb
Kartik Kher <kartikkher777@gmail.com>
2020-05-26 11:16:00 +0100

    Project Completed

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Listen for sumbit
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+  // Hide Results
+  document.getElementById("results").style.display = "none";
+  // Show Loader
+  document.getElementById("loading").style.display = "block";
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
 
-function calculateResults(e) {
+function calculateResults() {
   // UI Var
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
@@ -24,15 +29,21 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    // show results
+    document.getElementById("results").style.display = "block";
+    // hide loader
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please Check Your Numbers");
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // hide results
+  document.getElementById("results").style.display = "none";
+  // hide loader
+  document.getElementById("loading").style.display = "none";
   // create div
   const errorDiv = document.createElement("div");
   // get element
